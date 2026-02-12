1f67da86706b1ac3cc999b87dfb442c53510f078
Tolokoban <contact@tolokoban.org>
2026-02-12 11:22:54 +0100

    Editor in progress

--- src/app/05/editor/[levelIndex]/_/SelectionLayer/updater.ts
+++ src/app/05/editor/[levelIndex]/_/SelectionLayer/updater.ts
@@ -5,14 +5,13 @@ class LevelUpdater {}
 
 export function useLevelUpdater(levelIndex: number) {
     const { levels } = useArkanoidLevels()
-const [level, setLevel] = React.useState(levels[levelIndex])
-    const ref = React.useRef<LevelUpdater | null>(null)
-    if (!ref.current) ref.current = new LevelUpdater(setLevel)
+    // const [level, setLevel] = React.useState(levels[levelIndex])
+    //     const ref = React.useRef<LevelUpdater | null>(null)
+    //     if (!ref.current) ref.current = new LevelUpdater(setLevel)
 
-    React.useMemo(() => {
-        const l =  levels[levelIndex]
-        ref.current.
-        return l
-    }, [levels, levelIndex])
-    
+    //     React.useMemo(() => {
+    //         const l =  levels[levelIndex]
+    //         ref.current.
+    //         return l
+    //     }, [levels, levelIndex])
 }
